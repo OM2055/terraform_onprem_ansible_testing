7214b9c743685adbb6705466b23804a8b3558ebf
OM2055 <omkar.dba@gmail.com>
2024-09-12 14:58:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,17 @@
 # main.tf
+
 terraform {
-  required_providers {
-    tfe = {
-      source  = "hashicorp/tfe"
-      version = "~> 0.42.0"
+  required_providers {                     #### ansible provider
+    ansible = {
+      version = "~> 0.0.1"
+      source  = "terraform-ansible.com/ansibleprovider/ansible"
     }
+   # aws = {
+   #   source  = "hashicorp/aws"
+   #   version = "~> 4.0"
+   # }
   }
 }
-
-provider "tfe" {
-  hostname = "app.terraform.io"
-  token    = var.tfe_token
-}
-
-
 provider "tls" {}
 
 provider "local" {}
