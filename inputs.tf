685ea8099014efec4b1310a57c8fe27406ff2fd5
Omkar Singh <omkar.dba@outlook.com>
2024-09-16 17:40:55 +0530

    DOne

--- inputs.tf
+++ inputs.tf
@@ -1,5 +1,1 @@
 # inputs.tfvars
-
-vm_ip_address   = "119.252.222.135" # Replace with your VM's IP
-ssh_user        = "root"          # Replace with your SSH user
-private_key_path = "~/.ssh/id_rsa" # Path to your SSH private key
